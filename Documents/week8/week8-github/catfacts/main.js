4ee4e04addce498c9af520e1abdb276d6f95f965
Alexa Macias <Alexa Macias>
2021-10-02 20:21:56 -0400

    catfacts

---
+++ Documents/week8/week8-github/catfacts/main.js
@@ -0,0 +1,12 @@
+//Goal:  Retrieve a random cat fact when button is clicked.
+document.querySelector('button').addEventListener('click', getFact)
+function getFact(){
+    const url = `https://meowfacts.herokuapp.com/`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            document.querySelector('h3').innerHTML = data.data
+            console.log(data)
+        })
+        .catch(err => console.log(err))
+}
